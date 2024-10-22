cade3271d87d683fbc3078d55fee28ce8395bdc0
TomasDelfino <tomas.ez3010@gmail.com>
2024-10-22 20:15:31 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header.js';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <p>Bienvenido a mi aplicación!</p>
+        </div>
+    );
+}
+
+export default App;
